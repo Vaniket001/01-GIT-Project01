7acb47aac9c5beed790f48e14b81cdcb553aa646
Aniket <kumaraniket900@gmail.com>
2022-01-31 08:28:44 +0530

    Git First Commit 01

---
+++ src/com/coding/git/practice/GitCoding01.java
@@ -0,0 +1,19 @@
+package com.coding.git.practice;
+
+public class GitCoding01 {
+
+	public static void main(String[] args) {
+
+		// 01. Simple printing the name...
+		printName(3);
+
+	}
+
+	public static void printName(int no) {
+
+		for (int i = 1; i <= no; i++) {
+			System.out.println("Welcome to learn GIT Concept");
+		}
+	}
+
+}
