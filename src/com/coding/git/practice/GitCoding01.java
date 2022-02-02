46478ae933d079c498b2dc511b6edbfe38745d7e
Aniket <kumaraniket900@gmail.com>
2022-02-02 07:23:28 +0530

    Added a method of sum

--- src/com/coding/git/practice/GitCoding01.java
+++ src/com/coding/git/practice/GitCoding01.java
@@ -11,9 +11,13 @@ public class GitCoding01 {
 
 		// 01. Simple printing the name...
 		printName(3);
+		
+		//03. Sum og two numbers...
+		System.out.println("Sum of two numbers is :" + sum(5,11));
 
 	}
 
+	// 01.
 	public static void printName(int no) {
 
 		for (int i = 1; i <= no; i++) {
@@ -22,6 +26,11 @@ public class GitCoding01 {
 			System.out.println("Everything is ok");
 			System.out.println("Learning git stash command");
 		}
+	}
+	
+	//03.
+	public static int sum(int a, int b) {
+		return a + b;
 	}
 
 }
